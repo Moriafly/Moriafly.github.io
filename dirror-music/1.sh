2c93bfd4316665528de6c637de6b2f277f72672d
Moriafly <62319450+Moriafly@users.noreply.github.com>
2021-04-16 19:56:00 +0800

    2021年4月16日19:55:57

---
+++ dirror-music/1.sh
@@ -0,0 +1,6 @@
+SHOU // This is the OpenVPN installation script. Please continue to enter or ctrl+C to cancel 
+
+
+The initialization installation environment configuration is complete.
+
+docker-compose has installed.
--- dirror-music/cpp/1.cpp
+++ dirror-music/cpp/1.cpp
@@ -1,13 +1,56 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-class Date{
-    Date()
+static int STUDENT_COUNTS = 10;
+
+class Student{
     public:
-    
+        int id;
+        string name;
+        int score;
+        Student(int id, string name, int score) {
+            this->id = id;
+            this->name = name;
+            this->score = score;
+        }
+    private:
 };
 
 int main() {
-
+    Student student(0, "bob", 90);
+    
     return 0;
 }
+
+class M{
+	int A;
+	static int B;
+public:
+	M(int a)
+	{
+		A=a;
+		B+=a;
+		cout<<"Constructing"<<endl;
+	}
+    static void f1(M m);
+	~M()
+	{
+		cout<<"Destructing\n";
+	}
+};
+
+void M::f1(M m)
+	{
+		cout<<"A="<<m.A<<endl;
+		cout<<"B="<<B<<endl;
+	}
+	int M::B=0;
+
+int main()
+	{
+		M P(5),Q(10);
+		M::f1(P);
+		M::f1(Q);
+		return 0;
+	}
