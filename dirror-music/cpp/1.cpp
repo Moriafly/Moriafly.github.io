f7719cdf68725544fc646510657d0af3749c7ffc
Moriafly <62319450+Moriafly@users.noreply.github.com>
2021-04-29 13:50:31 +0800

    2021年4月29日13:50:28

--- dirror-music/cpp/1.cpp
+++ dirror-music/cpp/1.cpp
@@ -1,56 +1,67 @@
-#include <iostream>
 #include <cstring>
+#include <iostream>
 using namespace std;
 
 static int STUDENT_COUNTS = 10;
 
-class Student{
-    public:
-        int id;
-        string name;
-        int score;
-        Student(int id, string name, int score) {
-            this->id = id;
-            this->name = name;
-            this->score = score;
-        }
-    private:
+class Student {
+   public:
+    int id;
+    string name;
+    int score;
+    Student(int id, string name, int score) {
+        this->id = id;
+        this->name = name;
+        this->score = score;
+    }
+
+   private:
 };
 
 int main() {
     Student student(0, "bob", 90);
-    
+
     return 0;
 }
 
-class M{
-	int A;
-	static int B;
-public:
-	M(int a)
-	{
-		A=a;
-		B+=a;
-		cout<<"Constructing"<<endl;
-	}
+class M {
+    int A;
+    static int B;
+
+   public:
+    M(int a) {
+        A = a;
+        B += a;
+        cout << "Constructing" << endl;
+    }
     static void f1(M m);
-	~M()
-	{
-		cout<<"Destructing\n";
-	}
+    ~M() { cout << "Destructing\n"; }
 };
 
-void M::f1(M m)
-	{
-		cout<<"A="<<m.A<<endl;
-		cout<<"B="<<B<<endl;
-	}
-	int M::B=0;
+void start() {}
 
-int main()
-	{
-		M P(5),Q(10);
-		M::f1(P);
-		M::f1(Q);
-		return 0;
+void M::f1(M m) {
+    cout << "A=" << m.A << endl;
+    cout << "B=" << B << endl;
+}
+int M::B = 0;
+
+int main() {
+    
+	int a = 0;
+	
+
+	return 0;
+}
+
+int slite_number(int a, int b) {
+	int sum = 0;
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; j < b; j++) {
+			
+
+		}
 	}
+
+	return 0;
+}
